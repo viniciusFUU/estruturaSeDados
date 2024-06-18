3595addda13a16f3fd4739d60900fad1037fff3a
Vinícius Monteiro <107506027+viniciusFUU@users.noreply.github.com>
2024-06-18 11:11:44 -0300

    Aula02 (#2)
    * Criando listas e adicionando elemento de maneira otimizada
    
    * Otimizando padrões de busca de dados

--- src/main/java/com/Vetores.java
+++ src/main/java/com/Vetores.java
@@ -18,4 +18,18 @@ public class Vetores {
             throw new Exception("Limite do vetor ultrapassado.");
         }
     }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public String busca(int posicao) throws Exception{
+        if (!(posicao >= 0 && posicao < this.contador)){
+            throw new IllegalArgumentException("Posição inválida.");
+        }
+
+        return elementos[posicao];
+        
+    }
+
 }
